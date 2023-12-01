cf36b9703a56ed4618c36c391a632604c8e0b34e
gdrbl <github@guidorebel.nl>
2023-12-01 19:09:58 +0100

    AOC 2023 - day 1

--- day01/day01.py
+++ day01/day01.py
@@ -7,28 +7,19 @@ data: list[str] = puzzle.getStrList()
 words:  list[str] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 digits: list[str] = ["1","2","3","4","5","6","7","8","9"]
 
-FROMSTART = 1
-FROMEND   = 2
 
-def findDigit(line:str, fromStartorEnd: int) -> int:
+def findDigit(line:str, fromStart: bool, searchWords: bool) -> int:
 
-    if fromStartorEnd == FROMSTART:
-        start = 0
-        stop = len(line)
-        step = 1
+    if fromStart: r = range(len(line))
+    else:         r = range(len(line))[::-1]
 
-    elif fromStartorEnd == FROMEND:
-        start = len(line)-1
-        stop = -1
-        step = -1
-
-    for index in range(start, stop, step):
+    for index in r:
 
         for digit in digits:
             if line[index].find(digit) == 0:
                 return int(digit)
             
-        if not PARTONE:
+        if searchWords:
             for word in words:
                 if line[index::].find(word) == 0:
                     return int(digits[words.index(word)])
@@ -40,24 +31,23 @@ total: int
 firstdigit: int
 lastdigit: int
 
+# Part 1
 #===================
 
-PARTONE = True
 total = 0
 for line in data:
-    firstdigit = findDigit(line, FROMSTART)
-    lastdigit  = findDigit(line, FROMEND)
+    firstdigit = findDigit(line, True, False)
+    lastdigit  = findDigit(line, False, False)
     total += firstdigit * 10 + lastdigit
 print (total)
 
+
+# Part 2
 #===================
 
-PARTONE = False
 total = 0
 for line in data:
-    firstdigit = findDigit(line, FROMSTART)
-    lastdigit  = findDigit(line, FROMEND)
+    firstdigit = findDigit(line, True, True)
+    lastdigit  = findDigit(line, False, True)
     total += firstdigit * 10 + lastdigit
 print (total)
-
-#===================
